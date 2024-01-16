60aee2516b7eb97d021cba1d8c88f146c89e5ce6
Seiya Kojima <seiya.kojima@azone-japan.co.jp>
2024-01-16 10:47:15 +0900

    Serviceを追加

--- src/main/java/com/example/sample/SweetsController.java
+++ src/main/java/com/example/sample/SweetsController.java
@@ -1,76 +1,35 @@
 package com.example.sample;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.sample.dto.SweetsData;
-import com.example.sample.entity.Sweets;
 import com.example.sample.entity.SweetsForm;
-import com.example.sample.repository.SweetsRepository;
+import com.example.sample.service.SweetsService;
 
 @Controller
 public class SweetsController {
 
 	@Autowired
-	SweetsRepository repository;
+	private SweetsService service;
 	
 	@RequestMapping("/showcase")
 	public String showcase(Model model) {
-		List<Sweets> list = repository.findAll();
-		model.addAttribute("sweets", list);
+		model.addAttribute("sweets", service.getShowcaseData());
 		return "showcase";
 	}
-	
+
 	@RequestMapping("/shop")
 	public String shop(Model model) {
-		List<Sweets> list = repository.findAll();
-		// エンティティを画面データに詰めかえる
-		List<SweetsData> sweetsList = new ArrayList<SweetsData>();
-		for (Sweets sweets : list) {
-			SweetsData data = new SweetsData();
-			data.setId(sweets.getId());
-			data.setItem(sweets.getItem());
-			data.setKind(sweets.getKind());
-			data.setStock(sweets.getStock());
-			sweetsList.add(data);
-		}
-		SweetsForm sweetsForm = new SweetsForm();
-		sweetsForm.setSweetsList(sweetsList);
-		model.addAttribute("sweetsForm", sweetsForm);
-		
+		model.addAttribute("sweetsForm", service.getShopData());
 		return "shop";
 	}
 
 	@PostMapping("/buy")
 	public String buy(SweetsForm sweetsForm, Model model) {
-		// お買い上げリスト
-		List<Sweets> shoppingList = new ArrayList<Sweets>();
-		// チェックのついた商品のみお買い上げ
-		for (SweetsData sweets : sweetsForm.getSweetsList()) {
-			if (sweets.isChecked()) {
-				Sweets data = new Sweets();
-				data.setId(sweets.getId());
-				data.setItem(sweets.getItem());
-				data.setKind(sweets.getKind());
-				// 買われた商品の在庫数を減らす
-				if (sweets.getStock() > 1) {
-					data.setStock(sweets.getStock() - 1);
-				} else {
-					data.setStock(0);
-				}
-				shoppingList.add(data);
-				
-				// DB更新
-				repository.save(data);
-			}
-		}
-		model.addAttribute("sweets", shoppingList);
+		model.addAttribute("sweets", service.updateBuy(sweetsForm));
 		return "thanks";
 	}
 }
