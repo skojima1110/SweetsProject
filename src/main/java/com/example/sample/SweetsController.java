c8c0dfd0133a47d897c893160586ae6082982811
Seiya Kojima <seiya.kojima@azone-japan.co.jp>
2024-02-08 18:52:08 +0900

    課題４　商品が選択されていない場合の処理を追加

--- src/main/java/com/example/sample/SweetsController.java
+++ src/main/java/com/example/sample/SweetsController.java
@@ -1,11 +1,14 @@
 package com.example.sample;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.sample.dto.SweetsData;
 import com.example.sample.entity.SweetsForm;
 import com.example.sample.service.SweetsService;
 
@@ -29,8 +32,15 @@ public class SweetsController {
 
 	@PostMapping("/buy")
 	public String buy(SweetsForm sweetsForm, Model model) {
-		model.addAttribute("sweets", service.updateStock(sweetsForm));
-		return "thanks";
+		List<SweetsData> list = service.updateStock(sweetsForm);
+		if (list.size() > 0) {
+			model.addAttribute("sweets", list);
+			return "thanks";
+		} else {
+			model.addAttribute("sweetsForm", sweetsForm);
+			model.addAttribute("msg", "商品を選んでください");
+			return "shop";
+		}
 	}
 
 }
